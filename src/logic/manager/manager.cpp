a28a21e41ee576e14e395444ecfe7f45af465e17
Hans Goor <rooteddev@outlook.com>
2020-02-16 17:09:01 +0100

    Finalised all the logic.

--- src/logic/manager/manager.cpp
+++ src/logic/manager/manager.cpp
@@ -2,9 +2,9 @@
 // Created by eyedevelop on 03/02/2020.
 //
 
+#include "manager.h"
 #include <algorithm>
 #include <iostream>
-#include "manager.h"
 #include "../get_combination.cpp"
 
 /**
@@ -25,12 +25,27 @@ void Manager::remove_player(Player *const p) noexcept {
     this->players.erase(std::find(this->players.begin(), this->players.end(), p));
 }
 
+/**
+ * Gets the current player and calls the player's
+ * make_play function to determine the action to
+ * be taken for that player.
+ */
 void Manager::next_play() noexcept {
     Player *p_current_player = get_current_player();
 
+    // Check if a round has been concluded.
+    if (current_player == round_end_at) {
+        this->next_round();
+    }
+
+    // Game has to be active to make moves.
+    if (!this->active) {
+        return;
+    }
+
     // Check if the player has folded before.
     bool folded = false;
-    for (Player *const p : folded_players) {
+    for (Player *const p : this->folded_players) {
         if (p->get_name() == p_current_player->get_name()) {
             folded = true;
         }
@@ -42,42 +57,53 @@ void Manager::next_play() noexcept {
         return;
     }
 
-    // Check if a round has been concluded.
-    if (current_player == round_end_at) {
-        this->biggest_raise = 0;
-        this->next_round();
-    }
-
     // Let the player make their move.
-    double delta = p_current_player->make_play(this->pot);
-    if (delta == FOLD) {
-        folded_players.push_back(p_current_player);
-    } else if (delta > 0) {
-        this->pot += delta;
+    double action = p_current_player->make_play(this->pot, this->raise_delta);
+    if (action == FOLD) {
+        this->folded_players.push_back(p_current_player);
 
-        if (delta > biggest_raise)
+    } else if (action == CHECK) {
+        this->pot += this->biggest_raise;
+
+    } else if (action > 0) {
+        this->pot += action;
+
+        if (action > this->biggest_raise) {
+            this->raise_delta = action - this->biggest_raise;
+            this->biggest_raise = action;
             this->round_end_at = this->current_player;
+        }
     }
 
     // Increment player.
     this->current_player = (this->current_player + 1) % this->players.size();
 }
 
+/**
+ * Increments the round counter and places
+ * a card (or multiple) depending on the
+ * current round.
+ *
+ * Round 1: Betting on the just-given hand.
+ * Round 2: Three cards appear on the table.
+ * Round 3: Extra card gets placed.
+ * Round 4: Another extra card is placed. These are the final bets.
+ */
 void Manager::next_round() noexcept {
-    if (this->round_num >= 5) {
+    if (++this->round_num > 4) {
         this->active = false;
         return;
     }
 
-    if (this->round_num == 1) {
+    if (this->round_num == 2) {
         for (int i = 0; i < 3; i++) {
             this->table_cards.push_back(this->deck.next_card());
         }
-    } else if (this->round_num > 1) {
+    } else if (this->round_num > 2) {
         this->table_cards.push_back(this->deck.next_card());
     }
 
-    this->round_num ++;
+    this->biggest_raise = 0;
     this->round_end_at = 0;
 }
 
@@ -90,9 +116,13 @@ void Manager::next_round() noexcept {
  * added to their funds.
  */
 void Manager::start_game() noexcept {
+    // Cannot start a game with only one player.
+    if (this->players.size() < 2)
+        return;
+
     // Sets the players in-game and gives them cards.
     for (Player *const p : players) {
-        p->set_in_game();
+        p->set_in_game(&this->table_cards);
         p->give_card(this->deck.next_card());
         p->give_card(this->deck.next_card());
     }
@@ -124,7 +154,11 @@ void Manager::start_game() noexcept {
     for (Player *const p : players)
         p->reset_in_game();
 
-    // Reset the deck.
+    // Reset the manager.
+    this->current_player = 0;
+    this->round_end_at = 0;
+    this->biggest_raise = 0;
+    this->pot = 0;
     this->deck.reset();
 }
 
@@ -156,18 +190,37 @@ Manager::check_win(std::vector<Player *> const &active_players, std::vector<Card
     return winners;
 }
 
-std::vector<Player *> &Manager::get_players() noexcept {
+/**
+ * Gets all the players in this manager.
+ *
+ * @return Those players.
+ */
+std::vector<Player *> Manager::get_players() noexcept {
     return players;
 }
 
-Player *Manager::get_current_player() {
+/**
+ * Gets the current player as Player*.
+ *
+ * @return The player (as pointer) currently at turn.
+ */
+Player *Manager::get_current_player() noexcept {
     return this->players[current_player];
+}
+
+/**
+ * Gets the cards currently on the table.
+ *
+ * @return The cards on the table.
+ */
+std::vector<Card> Manager::get_cards() noexcept {
+    return this->table_cards;
 }
 
 int main() {
     Manager m{};
     Player p{"Hans", 1200};
-    Player p2{"John", 1200};
+    Player p2{"John", 1201};
 //    Player p3{"Rutger", 1200};
 
     m.add_player(&p);
--- src/logic/manager/manager.h
+++ src/logic/manager/manager.h
@@ -5,7 +5,6 @@
 #ifndef GODOTPOKER_MANAGER_H
 #define GODOTPOKER_MANAGER_H
 
-
 #include <vector>
 #include "../player/player.h"
 #include "../card/deck.h"
@@ -20,6 +19,7 @@ class Manager {
     size_t round_end_at = 0;
     size_t round_num = 0;
 
+    double raise_delta = 0;
     double biggest_raise = 0;
     double pot = 0;
 
@@ -32,7 +32,7 @@ public:
 
     void remove_player(Player *p) noexcept;
 
-    std::vector<Player *> &get_players() noexcept;
+    std::vector<Player *> get_players() noexcept;
 
     void next_play() noexcept;
 
@@ -40,7 +40,9 @@ public:
 
     void start_game() noexcept;
 
-    Player * get_current_player();
+    Player *get_current_player() noexcept;
+
+    std::vector<Card> get_cards() noexcept;
 
     static std::vector<Player *>
     check_win(std::vector<Player *> const &active_players, std::vector<Card> const &board_cards) noexcept;
